f78a11e6c94388d9569c6bd3fe853c8a10711096
Breno Estrella <brenogbestrella@gmail.com>
2022-03-21 09:47:54 -0300

    Primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter">
+      <FormularioTrack />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import FormularioTrack from "./components/FormularioTrack.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    FormularioTrack,
+  },
+});
+</script>
+
+<style></style>
